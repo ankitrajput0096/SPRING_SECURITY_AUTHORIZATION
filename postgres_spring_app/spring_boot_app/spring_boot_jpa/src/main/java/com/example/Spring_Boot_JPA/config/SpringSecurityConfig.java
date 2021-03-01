ff0d52407c68b75bbd803acb8ad24402f2bea132
Ankit_Rajput <ankitrajput0096@gmail.com>
2021-03-01 22:33:02 +0530

    unused code

--- postgres_spring_app/spring_boot_app/spring_boot_jpa/src/main/java/com/example/Spring_Boot_JPA/config/SpringSecurityConfig.java
+++ postgres_spring_app/spring_boot_app/spring_boot_jpa/src/main/java/com/example/Spring_Boot_JPA/config/SpringSecurityConfig.java
@@ -96,31 +96,4 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 });
     }
-
-    /*
-
-
-ankit@ankit-Inspiron-3542:~$ curl localhost:8080/books/2
-{"timestamp":"2021-01-24T09:35:05.517+0000","status":401,"error":"Unauthorized","message":"Authentication Failed, Yaar Ankit","path":"/books/2"}
-
-ankit@ankit-Inspiron-3542:~$ curl --request POST localhost:8080/books -u Ankit:rajput
-{"timestamp":"2021-01-24T09:35:26.074+0000","status":403,"error":"Forbidden","message":"Authorization Failed, Yaar Ankit : Access is denied","path":"/books"}ankit@ankit-Inspiron-3542:~$
-
-ankit@ankit-Inspiron-3542:~$ curl --request GET localhost:8080/books -u Ankit:rajput
-[{"id":1,"name":"A Guide to the Bodhisattva Way of Life","author":"Santideva","price":15.41},{"id":2,"name":"The Life-Changing Magic of Tidying Up","author":"Marie Kondo","price":9.69},{"id":3,"name":"Refactoring: Improving the Design of Existing Code","author":"Martin Fowler","price":47.99}]ankit@ankit-Inspiron-3542:~$
-
-     */
-
-
-    /*@Bean
-    public UserDetailsService userDetailsService() {
-        //ok for demo
-        AppUser.UserBuilder users = AppUser.withDefaultPasswordEncoder();
-
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("password").roles("USER").build());
-        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
-        return manager;
-    }*/
-
 }
